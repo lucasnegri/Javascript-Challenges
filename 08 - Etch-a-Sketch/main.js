2298abfc7303fc769f050e278d5f36efcfce9f3d
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2022-07-14 16:37:59 -0300

    Add reset grid function

--- 08 - Etch-a-Sketch/main.js
+++ 08 - Etch-a-Sketch/main.js
@@ -10,43 +10,49 @@ function makeGrid() {
         squares.classList.add("square");
         grid.appendChild(squares);
     }
+    
+    //Creating Draw Function//
+    square.addEventListener("mousemove", function(event) {
+        event.target.classList.replace("square", color);
+        event.target.classList.replace("bg-dark", color);
+        event.target.classList.replace("bg-light", color);
+        event.target.classList.replace("bg-info", color);
+        event.target.classList.replace("bg-danger", color);
+    });
 };
 makeGrid()
-
-//Creating Draw Function//
-square.addEventListener("mouseover", function(event) {
-    event.target.classList.replace("square", color);
-    event.target.classList.replace("bg-dark", color);
-    event.target.classList.replace("bg-light", color);
-    event.target.classList.replace("bg-info", color);
-    event.target.classList.replace("bg-danger", color);
-});
 
 
 //Creating Color Change//
 document.querySelector("#bg-dark").addEventListener("click", function(e) {
     color = 'bg-dark';
 });
-document.querySelector("#bg-light").addEventListener("click", function(e) {
+document.querySelector("#bg-white").addEventListener("click", function(e) {
     color = 'bg-light';
 });
-document.querySelector("#bg-info").addEventListener("click", function(e) {
+document.querySelector("#bg-blue").addEventListener("click", function(e) {
     color = 'bg-info';
 });
-document.querySelector("#bg-danger").addEventListener("click", function(e) {
+document.querySelector("#bg-red").addEventListener("click", function(e) {
     color = 'bg-danger';
+});
+document.querySelector("#bg-green").addEventListener("click", function(e) {
+    color = 'bg-success';
+});
+document.querySelector("#bg-yellow").addEventListener("click", function(e) {
+    color = 'bg-warning';
 });
 
 
-// //Creating Grid Reset//
+//Creating Grid Reset//
 
-// let reset = document.querySelector('#reset');
-// reset.addEventListener("click", clear);
-
-// function clear(){
-//     let squares = grid.querySelectorAll('div');
-//     squares.remove();
-// }
+let reset = document.querySelector('#reset').addEventListener("click", removeAllTiles);
+function removeAllTiles() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    makeGrid()
+}
 
 
 
