d21e6c6b8a17df1ef7610647e8d07cef971c5a1b
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2022-07-16 19:49:01 -0300

    Changed some classes and started the script

---
+++ 09 - Calculator/main.js
@@ -0,0 +1,17 @@
+    //Display//
+const result = document.querySelector('#result');
+const operator = document.querySelector('#operator');
+const num = document.querySelector('#number');
+    
+    //Buttons//
+/*Operator*/
+const clear = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
+const percent = document.querySelector('#percent');
+const divide = document.querySelector('#divide');
+const times = document.querySelector('#times');
+const minus = document.querySelector('#minus');
+const plus = document.querySelector('#plus');
+const equal = document.querySelector('#equal');
+/*Numbers*/
+const numbers = document.querySelectorAll('.num');
