98a042de63e4d554fb30828ee7e1d117cbc5b749
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2022-07-16 21:33:03 -0300

    Finalized UI

--- 09 - Calculator/main.js
+++ 09 - Calculator/main.js
@@ -1,17 +1,6 @@
-    //Display//
-const result = document.querySelector('#result');
-const operator = document.querySelector('#operator');
-const num = document.querySelector('#number');
-    
-    //Buttons//
-/*Operator*/
-const clear = document.querySelector('#clear');
-const backspace = document.querySelector('#backspace');
-const percent = document.querySelector('#percent');
-const divide = document.querySelector('#divide');
-const times = document.querySelector('#times');
-const minus = document.querySelector('#minus');
-const plus = document.querySelector('#plus');
-const equal = document.querySelector('#equal');
-/*Numbers*/
-const numbers = document.querySelectorAll('.num');
+
+const display = document.querySelector('.calculator-screen');
+const keys = document.querySelector('.calculator-keys');
+// operator
+// decimal
+// all-clear
