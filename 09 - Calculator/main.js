3b0f4a4938727fd92a12152eb6c7100ef4144ec6
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2022-07-17 14:08:19 -0300

    Script returning value of buttons

--- 09 - Calculator/main.js
+++ 09 - Calculator/main.js
@@ -1,6 +1,43 @@
 
-const display = document.querySelector('.calculator-screen');
-const keys = document.querySelector('.calculator-keys');
 // operator
 // decimal
 // all-clear
+
+const calculator = {
+    displayValue: '',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay(){
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue
+}
+updateDisplay();
+
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) =>{
+    const { target } = event;
+
+    if(!target.matches('button')){
+        return;
+    }
+
+    if(target.classList.contains('operator')){
+        console.log('operator', target.value);
+        return;
+    }
+    if(target.classList.contains('decimal')){
+        console.log('decimal', target.value);
+        return;
+    }
+    if(target.classList.contains('all-clear')){
+        console.log('clear', target.value);
+        return;
+    }
+    
+    console.log('digit', target.value);
+
+})
