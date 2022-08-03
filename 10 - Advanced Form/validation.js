ea9425eb1ff5c7a7c46dc807c6a23844c1363efe
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2022-08-02 23:48:10 -0300

    Fixed validation glitch
    After a really long week trying to figure out why the validation event listener wasn't working, i found out that classes of 
    no-valid inputs were being removed by Bootstrap. After that, i removed bootstrap validation system and programmed a javascript one, where, when a input is invalid, it add's a class that changes the input border to red.

---
+++ 10 - Advanced Form/validation.js
@@ -0,0 +1,53 @@
+
+/*EVENT PARA INPUTS*/
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+    })
+})
+
+
+function valida(input){
+    const tipoDeInput = input.dataset.tipo;
+
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid){
+        input.classList.remove('input-invalid')
+    } else {
+        input.classList.add('input-invalid')
+    }
+}
+
+
+/*DETERMINA CADA VALIDADOR PARA CADA DATA-SET DE INPUT*/
+const validadores = {
+    age:input => validaDataNascimento(input)
+}
+
+
+
+
+/*VALIDAÇÃO DATA NASCIMENTO*/
+
+function validaDataNascimento(input){
+    const dataRecebida = new Date (input.value)
+    let mensagem = '';
+
+    if(!maiorQue18(dataRecebida)){
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar. '
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data){
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
