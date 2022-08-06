edf3112a1ffd96e69242e99735f3d2f09f6febeb
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2022-08-06 15:50:35 -0300

    Fixed Next Step Button Glitch and validation phase

--- 10 - Advanced Form/main.js
+++ 10 - Advanced Form/main.js
@@ -1,8 +1,9 @@
 
 
-//CHANGE FORM TABS//
+                              //CHANGE FORM TABS//
 var currentTab = 0; 
-showTab(currentTab); 
+showTab(currentTab);
+validateTab(currentTab); 
 
 function showTab(n) {
   // This function will display the specified tab of the form ...
@@ -15,12 +16,15 @@ function showTab(n) {
     document.getElementById("btnPrev").style.display = "inline";
     }
     if (n == (x.length - 1)) {
+        document.getElementById("btnNext").type ="submit"
     document.getElementById("btnNext").innerHTML = "Submit";
-    
     } else {
     document.getElementById("btnNext").innerHTML = "Next";
+    document.getElementById("btnNext").type ="submit"
     }
   // ... and run a function that displays the correct step indicator:
+    fixStepIndicator(n)
+    validateTab(currentTab);
 }
 
 function nextPrev(n) {
@@ -28,16 +32,16 @@ function nextPrev(n) {
     var x = document.getElementsByClassName("tab");
     x[currentTab].style.display = "none";
     currentTab = currentTab + n;
- 
+    
     // if you have reached the end of the form... :
     if (currentTab >= x.length) {
-    //...the form gets submitted:
-        document.getElementById("myForm").submit();
-        return false;
+      //...the form gets submitted:
+    document.getElementById("myForm").submit();
+    return false;
     } 
     // Otherwise, display the correct tab:
-    showTab(currentTab);
     
+    showTab(currentTab);
 }
 
 
@@ -49,4 +53,124 @@ function fixStepIndicator(n) {
     }
     //... and adds the "active" class to the current step:
         x[n].className += " active";
+};
+
+function validateTab(currentTab) {
+    let allTabs = document.getElementsByClassName("tab");
+    let actualInputs = allTabs[currentTab].querySelectorAll('.form-control, .form-select, .confirmInfo');
+    const nextBtn = document.querySelector('#btnNext');
+    console.log(actualInputs)
+
+    for(i=0; i<actualInputs.length; i++){
+        if(actualInputs[i].checkValidity() === true){
+            nextBtn.removeAttribute('disabled')
+        } else {
+            nextBtn.setAttribute('disabled', 'disabled')
+        }
+    }
 }
+
+
+
+
+
+
+
+                //VALIDATION PART//
+
+/*EVENT PARA INPUTS*/
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target);
+        validateTab(currentTab)
+    })
+})
+
+
+function valida(input){
+    const tipoDeInput = input.dataset.tipo;
+    
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid){
+        input.classList.remove('input-invalid')
+    } else {
+        input.classList.add('input-invalid')
+    }
+}
+
+
+/*DETERMINA CADA VALIDADOR PARA CADA DATA-SET DE INPUT*/
+const validadores = {
+    age:input => validaDataNascimento(input),    
+    cep:input => recuperarCEP(input)
+}
+
+
+
+
+/*VALIDAÇÃO DATA NASCIMENTO*/
+
+function validaDataNascimento(input){
+    const dataRecebida = new Date (input.value)
+    let mensagem = '';
+
+    if(!maiorQue18(dataRecebida)){
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar. '
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data){
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
+
+
+
+
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+
+    if(!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data => {
+                if(data.erro) {
+                    input.classList.add('input-invalid')
+                    return
+                }
+                input.classList.remove('input-invalid')
+                preencheCamposComCEP(data)
+                return
+            }
+        )
+    }
+}
+
+function preencheCamposComCEP(data) {
+    const address= document.querySelector('[data-tipo="address"]');
+    const city = document.querySelector('[data-tipo="city"]');
+    const state = document.querySelector('[data-tipo="state"]');
+
+    address.value = data.logradouro
+    city.value = data.localidade
+    state.value = data.uf
+}
+
